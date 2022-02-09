33cad8d1361d2c717e5a3fd288e32ac0455de27f
Sahbi <sahbici1000@gmail.com>
2022-02-09 23:25:51 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,87 @@
+//spread operator
+// t = [1, 2, 3];
+// r = [1, 2, 3];
+// q = [1, 2, 3];
+// s = [1, 2, 3];
+
+// obj = { name: "sahbi", ID: "58e8a9z21e" };
+
+// res = { ...t, ...obj };
+
+// total = [...t, ...r, ...q, ...s];
+
+// console.log(total);
+
+// tMore = [
+//   ...t,
+//   "sahbi",
+//   "785",
+//   {
+//     name: "mohamed",
+//   },
+// ];
+// console.log(tMore);
+
+// //map
+// t = [...Array(5).keys()];
+// res = t.map((e) => e * 2);
+
+// console.log(res);
+
+// mappedArray = t.map((e) => e * 2);
+// console.log(mappedArray);
+
+// //filter
+// t = [0, 1, 2, 3, 4];
+// console.log(t);
+
+// evenNumbers = t.filter((element) => element % 2 === 0);
+
+// console.log(evenNumbers);
+
+// evenNumbers = t.filter((e) => e % 2 === 0);
+// console.log(evenNumbers);
+// menu = [
+//   {
+//     food: "🍔",
+//     price: 5000,
+//   },
+//   {
+//     food: "🍕",
+//     price: 2000,
+//   },
+//   {
+//     food: "🍟",
+//     price: 3000,
+//   },
+// ];
+
+//  cheapFood = menu.filter((e) => e.price < 4000);
+//  console.log(cheapFood);
+// //reduce
+// menu = [
+//   {
+//     food: "🍔",
+//     price: 5000,
+//   },
+//   {
+//     food: "🍕",
+//     price: 2000,
+//   },
+//   {
+//     food: "🍟",
+//     price: 3000,
+//   },
+// ];
+
+// total = menu.reduce((acc, currentOrder) => acc + currentOrder.price, 0);
+// console.log(total);
+console.log(1);
+
+setTimeout(() => {
+  console.log(2);
+}, 1000);
+
+
+
+console.log(3);
